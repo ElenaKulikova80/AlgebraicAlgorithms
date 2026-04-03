f5d6fa12c9541bf12f2592f60ee92d8fc676674d
KulikovaEV <kulikovaev@nknh.sibur.ru>
2026-04-03 23:38:23 +0300

    Добавила проверку val на ноль. Добавила проверку на переполнение значения.

--- AlgebraicAlgorithms/ExponentiationThroughBinaryDecompositionExponent.cs
+++ AlgebraicAlgorithms/ExponentiationThroughBinaryDecompositionExponent.cs
@@ -5,6 +5,8 @@
         public static double BinaryВecompositionExp(double val, uint exp)
         {
             double res = 1.0;
+            if (val == 0.0)
+                throw new ArgumentException("Значение не должно бфть равным нулю", nameof(val));
             double current = val;
 
             while (exp > 0)
@@ -15,6 +17,8 @@
                 }
 
                 current *= current;
+                if (double.IsInfinity(current) && exp > 1)
+                    throw new OverflowException("Переполнение промежуточного результата");
                 exp >>= 1;
             }
 
